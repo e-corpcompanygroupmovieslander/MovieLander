f70df0c7ee8121a40fad914be008a071420a3cf4
e-corpcompanygroupmovieslander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>
2023-03-08 08:44:56 +0300

    Delete Series.js
    Signed-off-by: e-corpcompanygroupmovieslander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>

--- Series.js
+++
@@ -1,8 +0,0 @@
-let SeriesData='\
-<a href="https://drive.google.com/uc?export=download&id=1-OlQVGBWPOS9IVU8cVFIpLkDNj2Q5lT5"><img loading="lazy" id="img" src="https://drive.google.com/uc?export=download&id=1IuWUMx6_nWbCO3sY38i_6CisuSOfkaoZ"></a>\
-<a href="https://drive.google.com/uc?export=download&id=17Pyo4Rom6ObnRwzMGdslDs9T49HPDFT1"><img loading="lazy" id="img" src="https://drive.google.com/uc?export=download&id=1dy7_FyRKt5Vd8SO39WE_JkXP4yX8dImL">\
-<a href="https://drive.google.com/uc?export=download&id=1GwOtgvXWoHz-m79umzEi-E5tHihRONfQ"><img loading="lazy" id="img" src="https://drive.google.com/uc?export=download&id=1VXe1VKpmdiBGKly61TV7gmTYYyFLnKU_">\
-'
-/*
-<a href=""><img loading="lazy" id="img" src=""></a>\
-*/
