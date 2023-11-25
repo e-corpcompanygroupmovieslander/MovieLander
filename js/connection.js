9a0aa604d18b8b29faffcaffc28d13668f39ce89
E-corp  Company Group Movie Lander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>
2023-11-26 01:55:01 +0300

    Update connection.js
    Signed-off-by: E-corp  Company Group Movie Lander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>

--- js/connection.js
+++ js/connection.js
@@ -36,7 +36,7 @@ const MONITORINGLINK=`https://script.google.com/macros/s/AKfycbzs1DXDZ81I9X8J4gN
 
 const CreateAccountlink=`https://script.google.com/macros/s/AKfycbxmSh3p2o97ORd3n9XVG5vloutvntdnp96_T47er84mAuLMvmIL-kc-VYtge-YGxyES/exec`;
 
-const NewVersion='7.3';
+const NewVersion='7.4';
 
 const UpatedNeeded='TRUE';
 
