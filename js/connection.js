2b5fba681d5d8d45880986fc26a9a6ae2903aba2
E-corp  Company Group Movie Lander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>
2023-11-27 09:16:24 +0300

    Update connection.js
    Signed-off-by: E-corp  Company Group Movie Lander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>

--- js/connection.js
+++ js/connection.js
@@ -34,7 +34,7 @@ const ADVERTSPATH=`https://e-corpcompanygroupmovieslander.github.io/MovieLander/
 
 const MONITORINGLINK=`https://script.google.com/macros/s/AKfycbz_L9FMiYhoZwpsSOr7aOFWb_9yYM3PcVBf2I8kVU2hpC3B7j-BP2Jj1js-E0DHjGwg/exec`;
 
-const CreateAccountlink=`https://script.google.com/macros/s/AKfycbydLELo7G8s_Uz3xxyT39qd1pFfIX3qAz-iwvbvzfqgTs7y6Fxw2P65Gh7VKu-Mythb/exec`;
+const CreateAccountlink=`https://script.google.com/macros/s/AKfycbwEBvGCOihKRv3sGzKTcGTSSXHcCniK06UKifMyoqAYF0xxqg28DB9fBeSClE2Kkh7d/exec`;
 
 const NewVersion='7.5';
 
