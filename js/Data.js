c4dec1ac7e7048a1afff0fa417c7b5c40f833744
E-corp  Company Group Movie Lander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>
2023-11-27 09:25:04 +0300

    Update Data.js
    Signed-off-by: E-corp  Company Group Movie Lander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>

--- js/Data.js
+++ js/Data.js
@@ -1,6 +1,6 @@
 const MONITORINGLINK=`https://script.google.com/macros/s/AKfycbz_L9FMiYhoZwpsSOr7aOFWb_9yYM3PcVBf2I8kVU2hpC3B7j-BP2Jj1js-E0DHjGwg/exec`;
 
-const CreateAccountlink=`https://script.google.com/macros/s/AKfycbwEBvGCOihKRv3sGzKTcGTSSXHcCniK06UKifMyoqAYF0xxqg28DB9fBeSClE2Kkh7d/exec`;
+const CreateAccountlink=`https://script.google.com/macros/s/AKfycbxmSh3p2o97ORd3n9XVG5vloutvntdnp96_T47er84mAuLMvmIL-kc-VYtge-YGxyES/exec`;
 
 const NewVersion='7.5';
 
